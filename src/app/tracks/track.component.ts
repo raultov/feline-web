c3ab1b16c44fbcce5dc4888fca122c7a84335002
raultov <angel.raul.tovar.martin@gmail.com>
2016-07-29 09:45:18 +0200

    added markers to route

--- src/app/tracks/track.component.ts
+++ src/app/tracks/track.component.ts
@@ -22,6 +22,11 @@ export class TrackComponent implements OnInit, OnDestroy, AfterViewInit {
     latCenter: number = 0.0;
     lngCenter: number = 0.0;
 
+    routePlanCoordinates = [];
+    routePath: any;
+    startingMarker: any;
+    endingMarker: any;
+
     private listenParameters: any;
     private subscribeGetPoints: any;
     private subscribeTrackCenter: any;
@@ -39,7 +44,7 @@ export class TrackComponent implements OnInit, OnDestroy, AfterViewInit {
         window['initMap'] = (ev) => {
             this.map = new google.maps.Map(document.getElementById('map'), {
                             center: {lat: this.latCenter, lng: this.lngCenter},
-                            zoom: 8
+                            zoom: 10
             });
 
             this.mapLoadedSubject.next(this.map);
@@ -53,7 +58,7 @@ export class TrackComponent implements OnInit, OnDestroy, AfterViewInit {
                     this.latCenter = (<any>result).latitude;
                     this.lngCenter = (<any>result).longitude;
 
-                    if (this.map === null) {
+                    if (this.map == null) {
                         this.mapLoadedSubject.subscribe(value => {
                              this.map.setCenter({lat: this.latCenter, lng: this.lngCenter});
                         });
@@ -76,7 +81,58 @@ export class TrackComponent implements OnInit, OnDestroy, AfterViewInit {
                 (result => {
                     this.points = result;
 
-                    // console.log(native);
+                    // Reset data
+                    this.routePlanCoordinates = [];
+
+                    if (this.routePath) {
+                        this.routePath.setMap(null);
+                    }
+
+                    if (this.startingMarker) {
+                        this.startingMarker.setMap(null);
+                    }
+
+                    if (this.endingMarker) {
+                        this.endingMarker.setMap(null);
+                    }
+
+                    // Fill route with points
+                    for (let i = 0; i < (<any>this.points).length; i++) {
+                        let point = this.points[i];
+                        this.routePlanCoordinates.push({lat: (<any>point).latitude, lng: (<any>point).longitude});
+                    }
+
+                    this.routePath = new google.maps.Polyline({
+                        path: this.routePlanCoordinates,
+                        geodesic: true,
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 2
+                    });
+
+                    this.startingMarker = new google.maps.Marker({
+                        position: this.routePlanCoordinates[0],
+                        title: 'Start',
+                        icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+                    });
+
+                    this.endingMarker = new google.maps.Marker({
+                        position: this.routePlanCoordinates[this.routePlanCoordinates.length - 1] ,
+                        title: 'End',
+                        icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+                    });
+
+                    if (this.map == null) {
+                        this.mapLoadedSubject.subscribe(value => {
+                             this.routePath.setMap(this.map);
+                             this.startingMarker.setMap(this.map);
+                             this.endingMarker.setMap(this.map);
+                        });
+                    } else {
+                            this.routePath.setMap(this.map);
+                            this.startingMarker.setMap(this.map);
+                            this.endingMarker.setMap(this.map);
+                    }
                 }),
                 (err => {
                     if (err.status === 401) {
